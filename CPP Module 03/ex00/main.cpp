4b97a12ca27a93d4de036994e9f09d51cd3c31d5
MrY2K <ayoubsamoraizi@gmail.com>
2026-02-12 17:22:29 +0100

    03

--- CPP Module 03/ex00/main.cpp
+++ CPP Module 03/ex00/main.cpp
@@ -1,23 +1,23 @@
 #include "ClapTrap.hpp"
 
-// int main() {
-//     ClapTrap test("CLAPPY");
+int main() {
+    ClapTrap test("CLAPPY");
 
-//     test.attack("xxHMIDAxx");
+    test.attack("xxHMIDAxx");
     
-//     test.takeDamage(5);
+    test.takeDamage(5);
     
-//     test.beRepaired(3);
+    test.beRepaired(3);
 
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
-//     test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
+    test.attack("Spam Target");
 
-//     return 0;
-// }
+    return 0;
+}
