8f75e0a4439eeb5debc4a0e746279d9d5cc30df3
MrY2K <ayoubsamoraizi@gmail.com>
2026-06-21 22:48:43 +0100

    cpp07ex01

--- {pr} CPP Module 07/ex02/Array.hpp
+++ {pr} CPP Module 07/ex02/Array.hpp
@@ -1,3 +1,42 @@
 #pragma once
 
+template <typename T>
+class Array
+{
+    private:
+        T* _elements;
+        unsigned int _size;
 
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(const Array& other);2
+        Array& operator=(const Array& other);
+        ~Array();
+
+};
+
+template <typename T>
+Array<T>::Array(): _elements(NULL), _size(_size = 0) {}
+
+template <typename T>
+Array<T>::Array(unsigned int n): _elements(new T[n]()), _size(n) {}
+
+template <typename T>
+Array<T>::Array(const Array& other)
+{
+
+}
+
+
+template <typename T>
+Array<T>::Array& operator=(const Array& other)
+{
+
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+    
+}
