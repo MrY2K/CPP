f012894dbd641b5415717e4cdc637b4279de43c3
MrY2K <ayoubsamoraizi@gmail.com>
2026-06-22 23:52:36 +0100

    cpp07

--- {pr} CPP Module 07/ex02/Array.hpp
+++ {pr} CPP Module 07/ex02/Array.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <exception>
+
 template <typename T>
 class Array
 {
@@ -10,14 +12,17 @@ class Array
     public:
         Array();
         Array(unsigned int n);
-        Array(const Array& other);2
+        Array(const Array& other);
         Array& operator=(const Array& other);
         ~Array();
+
+        T& operator[](unsigned int index);
+        unsigned int size() const;
 
 };
 
 template <typename T>
-Array<T>::Array(): _elements(NULL), _size(_size = 0) {}
+Array<T>::Array(): _elements(NULL), _size(0) {}
 
 template <typename T>
 Array<T>::Array(unsigned int n): _elements(new T[n]()), _size(n) {}
@@ -25,18 +30,52 @@ Array<T>::Array(unsigned int n): _elements(new T[n]()), _size(n) {}
 template <typename T>
 Array<T>::Array(const Array& other)
 {
+    _size = other._size;
+    _elements = new T[_size]();
 
+    for (unsigned int i = 0; i < _size; i++)
+    {
+        _elements[i] = other._elements[i];
+    }
 }
 
-
 template <typename T>
-Array<T>::Array& operator=(const Array& other)
+Array<T>& Array<T>::operator=(const Array& other)
 {
 
+    if ( this != &other )
+    {
+        delete[] _elements;
+
+        _size = other._size;
+        _elements = new T[_size]();
+
+        for (unsigned int i = 0; i < _size; i++)
+        {
+            _elements[i] = other._elements[i];
+        }
+    }
+    return *this;
 }
 
 template <typename T>
 Array<T>::~Array()
 {
-    
+    delete[] _elements;
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+    return _size;
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int index)
+{
+    if (index >= _size)
+        throw std::exception();
+
+    else
+        return _elements[index];
 }
